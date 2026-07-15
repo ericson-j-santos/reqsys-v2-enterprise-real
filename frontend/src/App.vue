bdc0c7ace079adf1dc1e40adf40232c07f7924d4
Ericson <60470500+ericson-j-santos@users.noreply.github.com>
2026-07-15 12:00:24 -0300

    feat(ux): montar estado vazio global de dashboards

--- frontend/src/App.vue
+++ frontend/src/App.vue
@@ -4,12 +4,14 @@
     <RouteFeedback v-if="$route.path !== '/login'" />
     <UserJourneyFeedback v-if="$route.path !== '/login'" />
     <GovBIEmptyStateBridge v-if="$route.path !== '/login'" />
+    <DashboardEmptyStateBridge v-if="$route.path !== '/login'" />
     <AppLayout v-if="$route.path !== '/login'" />
     <router-view v-else />
   </v-app>
 </template>
 <script setup>
 import AppLayout from './layouts/AppLayout.vue'
+import DashboardEmptyStateBridge from './components/DashboardEmptyStateBridge.vue'
 import GovBIEmptyStateBridge from './components/GovBIEmptyStateBridge.vue'
 import RouteFeedback from './components/RouteFeedback.vue'
 import UserExperienceGuardrails from './components/UserExperienceGuardrails.vue'
