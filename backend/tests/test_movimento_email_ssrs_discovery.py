80c916d168c58183fb814d08164437ebfcc3ee83
Ericson <60470500+ericson-j-santos@users.noreply.github.com>
2026-09-07 20:17:55 -0300

    test(movimento-email): validar descoberta segura de origem SSRS

---
+++ backend/tests/test_movimento_email_ssrs_discovery.py
@@ -0,0 +1,58 @@
+from pathlib import Path
+
+from app.services.movimento_email.ssrs_discovery import descobrir_origem_ssrs
+
+
+def test_descobre_servidor_banco_objetos_sem_expor_credenciais(tmp_path: Path):
+    rdl = tmp_path / 'Movimento.rdl'
+    rdl.write_text(
+        '''<?xml version="1.0" encoding="utf-8"?>
+<Report xmlns="http://schemas.microsoft.com/sqlserver/reporting/2016/01/reportdefinition">
+  <DataSources>
+    <DataSource Name="MovimentoDS">
+      <ConnectionProperties>
+        <DataProvider>SQL</DataProvider>
+        <ConnectString>Data Source=sql-prod-01;Initial Catalog=Movimento;User ID=svc;Password=nao-vazar</ConnectString>
+        <IntegratedSecurity>false</IntegratedSecurity>
+      </ConnectionProperties>
+    </DataSource>
+  </DataSources>
+  <DataSets>
+    <DataSet Name="Resumo">
+      <Query>
+        <DataSourceName>MovimentoDS</DataSourceName>
+        <CommandText>SELECT a.Id FROM dbo.Movimento a JOIN dbo.Cliente c ON c.Id=a.ClienteId</CommandText>
+      </Query>
+    </DataSet>
+  </DataSets>
+</Report>''',
+        encoding='utf-8',
+    )
+
+    resultado = descobrir_origem_ssrs(tmp_path)
+
+    assert resultado['servidores'] == ['sql-prod-01']
+    assert resultado['bancos'] == ['Movimento']
+    assert resultado['objetos_sql_candidatos'] == ['dbo.Cliente', 'dbo.Movimento']
+    assert 'UID=' not in resultado['dsn_molde_sem_credenciais']
+    assert 'Password=' not in resultado['dsn_molde_sem_credenciais']
+    assert 'nao-vazar' not in str(resultado)
+    assert resultado['credencial_exposta_no_resultado'] is False
+    assert resultado['detalhes'][0]['fontes'][0]['credencial_embutida_detectada'] is True
+
+
+def test_reporta_data_source_compartilhado_pendente(tmp_path: Path):
+    rdl = tmp_path / 'Movimento.rdl'
+    rdl.write_text(
+        '''<Report xmlns="http://schemas.microsoft.com/sqlserver/reporting/2016/01/reportdefinition">
+  <DataSources>
+    <DataSource Name="MovimentoDS"><DataSourceReference>/Dados/Movimento</DataSourceReference></DataSource>
+  </DataSources>
+</Report>''',
+        encoding='utf-8',
+    )
+
+    resultado = descobrir_origem_ssrs(rdl)
+
+    assert resultado['data_sources_compartilhados_pendentes'] == ['/Dados/Movimento']
+    assert resultado['dsn_molde_sem_credenciais'] is None
